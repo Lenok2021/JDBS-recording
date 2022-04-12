6c0f74c5f95556ab4119247eecf4444a4311bbe2
cross <crosstown11223@yahoo.com>
2022-04-12 13:13:48 -0400

    first commit

--- src/test/java/com/cydeo/day1/TestOracleConnection.java
+++ src/test/java/com/cydeo/day1/TestOracleConnection.java
@@ -6,69 +6,77 @@ import java.sql.*;
 public class TestOracleConnection {
 
     public static void main(String[] args) throws SQLException {
-
-        // step1: Create Connection by providing Connection Information
-        //  we provide JDBC url
-
-        String url = "jdbc:oracle:thin:@54.234.53.165:1521:XE"; // we get from developers
-        String userName = "hr";
-        String password = "hr";
+        // Step 1 : Create Connection by providing Connection information
+        // this connection jdbc url , also known as connection string
+        // syntax look like this jdbc:DataBaseType:subprotocol:@Host:port:SID
+        // PLEASE REPLACE THIS IP WITH YOUR OWN WORKING EC2 SERVER IP!!
+        String url = "jdbc:oracle:thin:@54.236.150.168:1521:XE" ;
+        String username = "hr" ;
+        String password = "hr" ;
 
         // now JDBC Provide utility class called DriverManager with many methods
         // the getConnection method accept 3 parameters url , username, password
         // according this information ,
         // it will first look for the driver of the database type you are connecting to
         // once it's found , use the rest of connection information like
-        // host ip , username password , port , sid to make connection
-        // MAKE SURE YOUR CONNECTION WORK MANUALLY ,
-        // ANYTHING DOES NOT WORK MANUALLY WILL NOT WORK AUTOMATION
+        //          host ip , username password , port , sid to make connection
+        // MAKE SURE YOUR CONNECTION WORKS MANUALLY ,
+        // ANYTHING THAT DOES NOT WORK MANUALLY WILL NOT WORK WITH AUTOMATION
 
-        //WE  WHEN  YOU LOOK DATA  from DATABASE  we need  DRIVER in this  way ORACLE  DRIVER
+        Connection conn = DriverManager.getConnection(url, username, password) ;
+        System.out.println("Successfully Connected");
 
-        Connection con = DriverManager.getConnection(url, userName, password);
-        System.out.println("Successfully connected");
+        // Once we have the connection , now we use this connection object to create statement object
+        Statement stmt = conn.createStatement() ;
 
-        // Once we have connection, now we use this connection Object to create statement Object
+        // Once we have Statement object , now we can execute query to get ResultSet
+        //  It stores the query result
+        // It uses iterator to move through the rows using cursor
+        // to hold position of where it's currently at
+        ResultSet rs = stmt.executeQuery("SELECT * FROM REGIONS") ;
+        // By default , the cursor(pointer) of the ResultSet object is at
+        // a location known as BeforeFirst , basically right before the first row
+        // in order to get to the first row, you need to call next() method
+        // next method move your pointer to next row
+        // it will return true if there is valid next row
+        // false if there is no more valid row
+        rs.next() ; // now we are at first row
 
-
-        Statement stmt = con.createStatement();
-
-        // Once we create statement Object, now we can execute query to get ResultSet
-
-        ResultSet rs = stmt.executeQuery("select * from regions");
-         // resultSet  Object store all the row data
-        // It uses iterator to move through the row using the cursor to hold its current position
-
-       // in order to get to the first row, you need to call next() method
-
-        rs.next() ; // now we are at first data
-        // now we need  to get  a data
-
-        // the resulting data has two column REGION_ID, REGION_NAME
-        // In order to access the cell value, we use getXmethod
-        // we can use column name or index
-        String firstRowValue = rs.getString("REGION_ID") ;
-        String secondRowValue = rs.getString("REGION_NAME") ;
-
-        System.out.println("firstRowValue = " + firstRowValue);
-        System.out.println("secondRowValue = " + secondRowValue);
-
-
-        System.out.println("Lets  find the row value by using column index number");
-
+        // The resulting data has two column REGION_ID , REGION_NAME
+        // In order to access the cell value ,
+        // we can use getX(X here is data type) method that accept 1 parameter
+        // either using column name or using 1 based index
+        // getString , getInt , getDouble and so on we will be just using getString for now
+        System.out.println("rs.getString(\"REGION_ID\") = "
+                + rs.getString("REGION_ID"));
+        System.out.println("rs.getString(\"REGION_NAME\") = "
+                + rs.getString("REGION_NAME"));
+        // you can also column index
         System.out.println("rs.getString(1) = " + rs.getString(1));
         System.out.println("rs.getString(2) = " + rs.getString(2));
 
-        // let's move to the next row
+        // now we want to move to next row
+        rs.next() ; // Now we are at 2rd row
+        System.out.println("rs.getString(\"REGION_ID\") = "
+                + rs.getString("REGION_ID"));
+        System.out.println("rs.getString(\"REGION_NAME\") = "
+                + rs.getString("REGION_NAME"));
 
-        rs.next() ;
+        // now we want to move to next row
+        rs.next() ; // Now we are at 3rd row
+        System.out.println("rs.getString(\"REGION_ID\") = "
+                + rs.getString("REGION_ID"));
+        System.out.println("rs.getString(\"REGION_NAME\") = "
+                + rs.getString("REGION_NAME"));
 
-        String firstRowValue1 = rs.getString("REGION_ID") ;
-        String secondRowValue1 = rs.getString("REGION_NAME") ;
-        System.out.println("firstRowValue1 = " + firstRowValue1);
-        System.out.println("secondRowValue1 = " + secondRowValue1);
+        // now we want to move to next row
+        rs.next() ; // Now we are at 4rd row
+        System.out.println("rs.getString(\"REGION_ID\") = "
+                + rs.getString("REGION_ID"));
+        System.out.println("rs.getString(\"REGION_NAME\") = "
+                + rs.getString("REGION_NAME"));
 
+        System.out.println("rs.next() = " + rs.next() );
+    }
     }
 
-
-}
